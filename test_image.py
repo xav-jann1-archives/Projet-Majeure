19ea8fbbab4370bf397251272c6d8c24baa014e0
xav-jann1 <xav-jann1@gmail.com>
2019-06-03 15:43:46 +0200

    Teste une image avec le chemin en argument

--- test_image.py
+++ test_image.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
+import sys
 
 # Scripts:
 from image_recognition import getLabelFromFilename
 
 
-
 def main():
   filename = './img.jpg'
+
+  # Si un chemin est donnée en paramètre:
+  if len(sys.argv) >= 2: filename = sys.argv[1]
+
   label = getLabelFromFilename(filename)
   print(label)
 
