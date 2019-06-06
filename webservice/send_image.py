0a309dd75775fa27114029a0ccfd352846b65f9f
xav-jann1 <xav-jann1@gmail.com>
2019-06-06 09:07:35 +0200

    Ajout d'un simple webservice

---
+++ webservice/send_image.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import cv2
+import httplib
+#import http.client as httplib  # Python 3
+
+# Connection au serveur:
+conn = httplib.HTTPConnection('127.0.0.1:5000', timeout=5)
+
+# Envoie d'une requête pour récupérer les labels d'une image:
+def getLabelsFromImage(img):
+  # Si img est le chemin de l'image:
+  if type(img) is str:
+    # Charge l'image:
+    img = cv2.imread(img)
+
+  # Encode l'image pour l'envoyer:
+  imencoded = cv2.imencode('.jpg', img)[1]
+
+  # Requête:
+  headers = {"Content-type": "test/plain"}
+  try:
+    conn.request("GET", "/labelImage", imencoded.tostring(), headers)
+    response = conn.getresponse()
+  except conn.timeout as e:
+    print("timeout")
+
+  return response.read()
+
+if __name__ == '__main__':
+  # Charge l'image:
+  img = cv2.imread('img_banane.jpg')
+
+  # Récupère les labels de l'image:
+  labels = getLabelsFromImage(img)
+  print(labels)
